708a6d1cbdbd0b663a084b2ec13cfa9f6ada16a0
bryan273 <bryan.tjandra@ui.ac.id>
2022-07-28 23:42:22 +0700

    Update jawaban 1 2

---
+++ SQL/Quiz/jawaban-2.sql
@@ -0,0 +1,21 @@
+-- Set only full group by
+SET sql_mode=(SELECT CONCAT(@@sql_mode, ',ONLY_FULL_GROUP_BY'));
+
+with temp as(
+	select td.no_do, ts.kode_customer, td.tgl_do, 
+		   mp.harga_satuan, mc.ongkos_kirim,
+	case when ts.satuan="krat" then ts.qty*24
+	when ts.satuan="dus" then ts.qty*30
+	else ts.qty end as qty
+	from tr_do td
+	join tr_so ts
+	using(no_entry_so)
+	join ms_customer mc 
+	using(kode_customer)
+	join ms_product mp 
+	on ts.kode_barang = mp.kode_produk
+	order by 1
+)
+select no_do, kode_customer, tgl_do, qty,
+	   round(1.1*qty*(harga_satuan)+ongkos_kirim) as amount
+from temp 
